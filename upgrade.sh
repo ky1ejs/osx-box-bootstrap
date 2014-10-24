3cfd38b93e930df831b9ce6d83d81ce2029347a4
Viktor Benei <viktor.benei@gmail.com>
2014-10-24 23:51:02 +0200

    update script now used the print-and-exit-on-error function

--- upgrade.sh
+++ upgrade.sh
@@ -5,31 +5,39 @@
 #
 
 function print_and_do_command {
-  echo "$ $@"
-  $@
+	echo "-> $ $@"
+	$@
+}
+
+function print_and_do_command_exit_on_error {
+	print_and_do_command $@
+	if [ $? -ne 0 ]; then
+		echo " [!] Failed!"
+		exit 1
+	fi
 }
 
 echo "--> Starting system refresh..."
-print_and_do_command brew update
-print_and_do_command brew doctor
-print_and_do_command brew upgrade
+print_and_do_command_exit_on_error brew update
+print_and_do_command_exit_on_error brew doctor
+print_and_do_command_exit_on_error brew upgrade
 #
-print_and_do_command rvm osx-ssl-certs update all
-print_and_do_command rvm get stable
-print_and_do_command rvm osx-ssl-certs update all
-source ~/.bash_profile
+print_and_do_command_exit_on_error rvm osx-ssl-certs update all
+print_and_do_command_exit_on_error rvm get stable
+print_and_do_command_exit_on_error rvm osx-ssl-certs update all
+print_and_do_command_exit_on_error source ~/.bash_profile
 #
-print_and_do_command gem update --system
-print_and_do_command gem update
+print_and_do_command_exit_on_error gem update --system
+print_and_do_command_exit_on_error gem update
 #
-print_and_do_command gem install cocoapods
-print_and_do_command pod setup
+print_and_do_command_exit_on_error gem install cocoapods
+print_and_do_command_exit_on_error pod setup
 #
 echo "<-- Finished with system refresh"
 
 
 echo "--> Starting system upgrade..."
-print_and_do_command sudo softwareupdate --install --all
+print_and_do_command_exit_on_error sudo softwareupdate --install --all
 # print the current OS version, for debugging
-print_and_do_command sw_vers
+print_and_do_command_exit_on_error sw_vers
 echo "<-- Finished with system upgrade"
