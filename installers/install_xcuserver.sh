dd009ffc260ef4cf4dc31bb8817e9dc50dabef01
Viktor Benei <viktor.benei@gmail.com>
2015-01-22 01:22:19 +0100

    remove the xcuserver folder if it exists to do a clean 'git clone'; and a bit of debug logging

--- installers/install_xcuserver.sh
+++ installers/install_xcuserver.sh
@@ -23,12 +23,19 @@
   CONFIG_xcuserver_git_repo_url="https://github.com/bitrise-io/xcodebuild-unittest-miniserver.git"
   CONFIG_xcuserver_git_version_tag="1.1.1"
 
+  echo
+  echo "===> Installing XCUServer..."
+
   echo " (i) xcuserver_base_dir: ${xcuserver_base_dir}"
   echo " (i) xcuserver_install_script_path: ${xcuserver_install_script_path}"
   echo " (i) CONFIG_xcuserver_git_repo_url: ${CONFIG_xcuserver_git_repo_url}"
   echo " (i) CONFIG_xcuserver_git_version_tag: ${CONFIG_xcuserver_git_version_tag}"
 
   print_and_do_command_exit_on_error mkdir -p "${bitrise_tools_dir}"
+
+  if [ -d "${xcuserver_base_dir}" ] ; then
+    print_and_do_command_exit_on_error rm -rf "${xcuserver_base_dir}"
+  fi
   print_and_do_command_exit_on_error git clone --single-branch -b "${CONFIG_xcuserver_git_version_tag}" "${CONFIG_xcuserver_git_repo_url}" "${xcuserver_base_dir}"
   print_and_do_command_exit_on_error bash "${xcuserver_install_script_path}"
 
