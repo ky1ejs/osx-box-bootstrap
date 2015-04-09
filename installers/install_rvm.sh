397128daff271b90b14a28c8dfb2e048b426fa58
Viktor Benei <viktor.benei@gmail.com>
2015-04-10 00:52:13 +0200

    couple of RVM related updates & replaced with bash -l -c it's previous, longer form

--- installers/install_rvm.sh
+++ installers/install_rvm.sh
@@ -3,7 +3,27 @@
 (
 	echo
 	echo "---> [install] RVM"
-	curl -sSL https://get.rvm.io | bash -s stable --ruby
+	tmp_rvm_install_script_path="$HOME/rvm-install-tmp.sh"
+	echo " (i) tmp_rvm_install_script_path: ${tmp_rvm_install_script_path}"
+
+	\curl -sSL -o "${tmp_rvm_install_script_path}" https://get.rvm.io
+	if [ $? -ne 0 ] ; then
+		echo " [!] Error: failed to download rvm installer"
+		exit 1
+	fi
+
+	cat "${tmp_rvm_install_script_path}" | bash -s stable
+	if [ $? -ne 0 ] ; then
+		echo " [!] Error: failed to do the RVM install"
+		exit 1
+	fi
+
+	rm "${tmp_rvm_install_script_path}"
+	if [ $? -ne 0 ] ; then
+		echo " [!] Error: failed to remove the temporary RVM install script file"
+		exit 1
+	fi
+
 	res_code=$?
 	echo "---> [install] RVM [done]"
 
