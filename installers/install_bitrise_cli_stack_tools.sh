0540cec3e8e27c15723aa5e87abe87ba097d5c3b
Viktor Benei <viktor.benei@gmail.com>
2015-09-02 20:43:44 +0200

    `bitrise-bridge` upgrade to 0.9.2

--- installers/install_bitrise_cli_stack_tools.sh
+++ installers/install_bitrise_cli_stack_tools.sh
@@ -20,7 +20,7 @@
 	# bitrise-bridge
 	echo
 	echo "[bitrise-bridge] Downloading..."
-	curl -L https://github.com/bitrise-io/bitrise-bridge/releases/download/0.9.1/bitrise-bridge-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise-bridge
+	curl -L https://github.com/bitrise-io/bitrise-bridge/releases/download/0.9.2/bitrise-bridge-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise-bridge
 	echo "[bitrise-bridge] chmod"
 	chmod +x /usr/local/bin/bitrise-bridge
 	echo "[bitrise-bridge] version"
