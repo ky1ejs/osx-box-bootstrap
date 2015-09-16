88de929d2f87777ca5560537a1980c52c9412674
Viktor Benei <viktor.benei@gmail.com>
2015-09-16 12:39:35 +0200

    pre-installed Bitrise CLI version and `bitrise-bridge` version upgrade & a couple of new Environments for keychain and `bitrise-bridge`

--- installers/install_bitrise_cli_stack_tools.sh
+++ installers/install_bitrise_cli_stack_tools.sh
@@ -9,7 +9,7 @@
 	# bitrise
 	echo
 	echo "[bitrise] Downloading..."
-	curl -L https://github.com/bitrise-io/bitrise/releases/download/0.9.11/bitrise-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise
+	curl -L https://github.com/bitrise-io/bitrise/releases/download/1.0.0/bitrise-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise
 	echo "[bitrise] chmod"
 	chmod +x /usr/local/bin/bitrise
 	echo "[bitrise] setup"
@@ -24,7 +24,7 @@
 	# bitrise-bridge
 	echo
 	echo "[bitrise-bridge] Downloading..."
-	curl -L https://github.com/bitrise-io/bitrise-bridge/releases/download/0.9.2/bitrise-bridge-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise-bridge
+	curl -L https://github.com/bitrise-io/bitrise-bridge/releases/download/0.9.4/bitrise-bridge-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise-bridge
 	echo "[bitrise-bridge] chmod"
 	chmod +x /usr/local/bin/bitrise-bridge
 	echo "[bitrise-bridge] version"
